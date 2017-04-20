095d1a12112f32331ff5a3b2f57966da91aa3272
waseemm <wmohd@CHIC02QRGD3G8WN.sea.corp.expecn.com>
2017-04-19 22:30:14 -0500

    Milestone 3

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var tray: UIView!
-    
-    
+
+
     var trayOriginalCenter: CGPoint!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    var trayDownOffset: CGFloat!
+
+    @IBOutlet weak var downArrow: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        trayDownOffset = 160
+        trayUp = tray.center
+        trayDown = CGPoint(x: tray.center.x ,y: tray.center.y + trayDownOffset)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        downArrow.addGestureRecognizer(tap)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,11 +34,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func didTap() {
+        self.tray.center = CGPoint(x: tray.center.x, y: 65)
+    }
+
     @IBAction func pan(_ sender: UIPanGestureRecognizer) {
         let pan = sender
         let translation = sender.translation(in: tray)
-        let point = pan.location(in: tray)
-        
+        let velocity = sender.velocity(in: view)
+        let point = pan.location(in: self.view)
+
         if pan.state == .began {
             trayOriginalCenter = tray.center
             print("Gesture began at: \(point)")
@@ -36,10 +51,19 @@ class ViewController: UIViewController {
             tray.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
             print("Gesture changed at: \(point)")
         } else if pan.state == .ended {
-            
-            print("Gesture ended at: \(point)")
+//            if velocity.y > 0 {
+//                UIView.animate(withDuration: 0.5) {
+//                    self.tray.center = self.trayDown
+//                }
+//            } else {
+//                UIView.animate(withDuration: 0.5) {
+//                    self.tray.center = self.trayUp
+//                }
+            tray.center = CGPoint(x: trayOriginalCenter.x, y: point.y)
+                print("Gesture ended at: \(point)")
+            }
         }
     }
 
-}
+
 
